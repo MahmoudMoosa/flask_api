d0e90ba83eaa6850282552d623f5007a8657f30c
MahmoudMoosa <124046942+MahmoudMoosa@users.noreply.github.com>
2024-03-13 01:55:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ def transcribe():
         return jsonify({'error': 'No text provided'}), 400
 
 if __name__ == '__main__':
-    serve(app, host='127.0.0.1', port=5000, threads=2)
+    serve(app, host='0.0.0.0', port=5000, threads=2)
